ea935e9e27e3dce2193cd0344867a5e9adee11e5
Bryan Van Mechelen <bryan.van.mechelen@hotmail.com>
2020-02-27 19:30:37 +0100

    Added HTML, Added PHP, Added array to PHP

---
+++ todo/php.php
@@ -0,0 +1,38 @@
+<?php
+
+$todos = [
+    "items" => 
+    [
+        "desc"      => ["Water plants"],
+        "duration"  => ["0.1"],
+        "category"  => ["Home"]
+    ],
+    [
+        "desc"      => ["Write Essay"],
+        "duration"  => ["1.5"],
+        "category"  => ["School"]
+    ],
+    [
+        "desc"      => ["Feed Cat"],
+        "duration"  => ["0.1"],
+        "category"  => ["Home"]
+    ],
+    [
+        "desc"      => ["Cook food"],
+        "duration"  => ["1"],
+        "category"  => ["Home"]
+    ],
+    [
+        "desc"      => ["Mail boss"],
+        "duration"  => ["0.4"],
+        "category"  => ["Work"]
+    ]
+];
+
+foreach($todos as $item){
+    foreach($item['duration'] as $duration){
+        
+    }
+}
+
+?>
